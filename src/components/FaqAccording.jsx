6e77de510381b3d03cc6307cdae7202c1877e275
SunilVerma0358 <sunilverma93060@gmail.com>
2024-05-06 22:57:25 +0530

    page complete

--- src/components/FaqAccording.jsx
+++ src/components/FaqAccording.jsx
@@ -4,7 +4,7 @@ import { FaqMap } from "./common/Helper";
 const FaqAccording = () => {
   return (
     <div className="py-5 my-md-5 ">
-      <div className="container pt-4">
+      <div className="container pt-4 pb-lg-5 mb-lg-5">
         <h2 className="robotoFlex fs_xl lh_xs fw-medium mb-0 text-center pb-2">
           Frequently Asked <span className="fw-bold greenBlue">Questions</span>
         </h2>
@@ -32,7 +32,7 @@ const FaqAccording = () => {
                   }`}
                   data-bs-parent="#accordionExample"
                 >
-                  <div className="accordion-body  open fw-normal lh_xxl fs_xs maxWidthPhara">
+                  <div className="accordion-body px-0 open fw-normal black lh_xxl fs_xs maxWidthPhara">
                     {props.phara}
                   </div>
                 </div>
